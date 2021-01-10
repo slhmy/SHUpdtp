cc9011cabb963b55ef9dfdad441c0ade0fedfe7c
slhmy <1484836413@qq.com>
2021-01-10 10:53:51 +0800

    add related data (works with db，remove this folder if you don't have official db)

---
+++ data/test_cases/1/spj_src.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define AC 0
+#define WA 1
+#define ERROR -1
+
+int spj(FILE *input, FILE *user_output);
+
+void close_file(FILE *f){
+    if(f != NULL){
+        fclose(f);
+    }
+}
+
+int main(int argc, char *args[]){
+    FILE *input = NULL, *user_output = NULL;
+    int result;
+    if(argc != 3){
+        printf("Usage: spj x.in x.out\n");
+        return ERROR;
+    }
+    input = fopen(args[1], "r");
+    user_output = fopen(args[2], "r");
+    if(input == NULL || user_output == NULL){
+        printf("Failed to open output file\n");
+        close_file(input);
+        close_file(user_output);
+        return ERROR;
+    }
+
+    result = spj(input, user_output);
+    printf("result: %d\n", result);
+
+    close_file(input);
+    close_file(user_output);
+    return result;
+}
+
+int spj(FILE *input, FILE *user_output){
+    return AC;
+}
