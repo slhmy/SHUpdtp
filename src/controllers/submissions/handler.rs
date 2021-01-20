dbc6dd2f3e9482de9de55db1da37fef506382f0a
slhmy <1484836413@qq.com>
2021-01-20 21:39:17 +0800

    update and and control methods

--- src/controllers/submissions/handler.rs
+++ src/controllers/submissions/handler.rs
@@ -1,8 +1,10 @@
 use actix_web::{web, HttpResponse, get, post, put};
 use crate::errors::ServiceError;
-use crate::database::Pool;
+use crate::database::{ db_connection, Pool };
 use crate::services::submission;
 use crate::models::users::LoggedUser;
+use crate::judge_actor::JudgeActorAddr;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct CreateSubmissionBody {
@@ -17,6 +19,7 @@ pub async fn create(
     body: web::Json<CreateSubmissionBody>,
     pool: web::Data<Pool>,
     logged_user: LoggedUser,
+    judge_actor: web::Data<JudgeActorAddr>,
 ) -> Result<HttpResponse, ServiceError> {
     info!("{:?}", logged_user.0);
     if logged_user.0.is_none() { return Err(ServiceError::Unauthorized); }
@@ -27,7 +30,43 @@ pub async fn create(
         logged_user.0.unwrap().id,
         body.src.clone(),
         body.language.clone(),
-        pool
+        pool,
+        judge_actor,
+    )).await.map_err(|e| {
+        eprintln!("{}", e);
+        e
+    })?;
+
+    Ok(HttpResponse::Ok().json(&res))
+}
+
+#[get("/{id}")]
+pub async fn get(
+    web::Path(submission_id): web::Path<Uuid>,
+    logged_user: LoggedUser,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, ServiceError> {
+    if logged_user.0.is_none() { return Err(ServiceError::Unauthorized); }
+    let cur_user = logged_user.0.unwrap();
+
+    let conn = &db_connection(&pool)?;
+
+    use crate::schema::submissions as submissions_schema;
+    use diesel::prelude::*;
+
+    let user_id: i32 = submissions_schema::table
+        .filter(submissions_schema::id.eq(submission_id))
+        .select(submissions_schema::user_id)
+        .first(conn)?;
+
+    if cur_user.id != user_id && cur_user.role != "super" && cur_user.role != "admin" {
+        let hint = "No permission.".to_string();
+        return  Err(ServiceError::BadRequest(hint));
+    }
+
+    let res = web::block(move || submission::get(
+        submission_id,
+        pool,
     )).await.map_err(|e| {
         eprintln!("{}", e);
         e
--- src/controllers/submissions/mod.rs
+++ src/controllers/submissions/mod.rs
@@ -6,5 +6,6 @@ pub fn route(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/submissions")
             .service(handler::create)
+            .service(handler::get)
     );
 }
