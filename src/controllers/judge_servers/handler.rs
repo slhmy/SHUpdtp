87ff402b4949816fcf09ecef77180b594aafe6b2
1987258436@qq.com <1987258436@qq.com>
2021-03-27 19:19:51 +0800

    ✨feat(judge_server): add API

--- src/controllers/judge_servers/handler.rs
+++ src/controllers/judge_servers/handler.rs
@@ -1,7 +1,11 @@
 use crate::errors::ServiceError;
 use crate::judge_actor::JudgeActorAddr;
 use crate::services::judge_server::*;
-use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse};
+use actix_identity::Identity;
+use actix_multipart::Multipart;
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use futures::{StreamExt, TryStreamExt};
+use std::io::Write;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct HeartbeatBody {
@@ -19,7 +23,6 @@ struct HeartbeatResponse {
     error: Option<String>,
 }
 
-#[post("/heartbeat")]
 pub async fn handle_heartbeat(
     body: web::Json<HeartbeatBody>,
     req: HttpRequest,
@@ -52,4 +55,42 @@ pub async fn handle_heartbeat(
             data: "success".to_owned(),
             error: None,
         }))
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct SubmitRequestForm {
+    pub problem_id: i32,
+    pub problem_region: String,
+    pub src: String,
+    pub language: String,
+    pub judge_type: String,
+    pub output: bool,
+}
+
+pub async fn get_server_info(id: Identity) -> Result<HttpResponse, ServiceError> {
+    server_info(id)
+        .await
+        .map(|res| HttpResponse::Ok().json(&res))
+}
+
+pub async fn get_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    // iterate over multipart stream
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("data/tmp/{}", sanitize_filename::sanitize(&filename));
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    Ok(HttpResponse::Ok().into())
 }
--- src/controllers/judge_servers/mod.rs
+++ src/controllers/judge_servers/mod.rs
@@ -3,5 +3,10 @@ pub mod handler;
 use actix_web::web;
 
 pub fn route(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/judge_servers").service(handler::handle_heartbeat));
+    cfg.service(
+        web::scope("/judge_server")
+            .service(web::resource("/heartbeat").route(web::post().to(handler::handle_heartbeat)))
+            .service(web::resource("/info").route(web::post().to(handler::get_server_info)))
+            .service(web::resource("/get_file").route(web::post().to(handler::get_file)))
+    );
 }
