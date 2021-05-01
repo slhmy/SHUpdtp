5c08cfba1de74c677486af91269c2cf4efad285a
slhmy <1484836413@qq.com>
2021-05-01 21:59:55 +0800

    fix route name

--- src/controllers/contests/mod.rs
+++ src/controllers/contests/mod.rs
@@ -4,7 +4,7 @@ use actix_web::web;
 
 pub fn route(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/region")
+        web::scope("/contests")
             .service(handler::create)
             .service(handler::insert_problems)
             .service(handler::get_contest_list)
