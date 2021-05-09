db7c54e2457c758656f38d8376af8027105c346d
1987258436@qq.com <1987258436@qq.com>
2021-05-09 22:01:48 +0800

    feat(problem_sets): add update function

--- src/controllers/problem_sets/handler.rs
+++ src/controllers/problem_sets/handler.rs
@@ -92,3 +92,43 @@ pub async fn delete(
 
     Ok(HttpResponse::Ok().json(&res))
 }
+
+#[derive(Deserialize)]
+pub struct UpdateProblemSetBody {
+    new_title: Option<String>,
+    new_introduction: Option<String>,
+}
+
+#[put("/{region}")]
+pub async fn update(
+    web::Path(region): web::Path<String>,
+    body: web::Json<UpdateProblemSetBody>,
+    pool: web::Data<Pool>,
+    logged_user: LoggedUser,
+) -> Result<HttpResponse, ServiceError> {
+    info!("{:?}", logged_user.0);
+    if logged_user.0.is_none() {
+        return Err(ServiceError::Unauthorized);
+    }
+    let cur_user = logged_user.0.unwrap();
+    if cur_user.role != "sup" && cur_user.role != "admin" {
+        let hint = "No permission.".to_string();
+        return Err(ServiceError::BadRequest(hint));
+    }
+
+    let res = web::block(move || {
+        problem_set::update(
+            region.clone(),
+            body.new_title.clone(),
+            body.new_introduction.clone(),
+            pool,
+        )
+    })
+    .await
+    .map_err(|e| {
+        eprintln!("{}", e);
+        e
+    })?;
+
+    Ok(HttpResponse::Ok().json(&res))
+}
